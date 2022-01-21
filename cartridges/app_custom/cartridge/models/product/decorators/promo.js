76365ff69259f611274fae1a48c202e2af0afb7d
YavorBorissov <yavor.borissov@gmail.com>
2022-01-21 14:30:53 +0200

    fix

--- cartridges/app_custom/cartridge/models/product/decorators/promo.js
+++ cartridges/app_custom/cartridge/models/product/decorators/promo.js
@@ -1,15 +1,8 @@
 "use strict";
-// function isPromo(promoMessage) {
-//   return promoMessage === "PROMOTION";
-// }
 
 module.exports = function (object, product) {
   Object.defineProperty(object, "promoMessage", {
     enumerable: false,
     value: product.custom.promoMessage,
   });
-  //   Object.defineProperty(object, "isPromo", {
-  //     enumerable: false,
-  //     value: isPromo(product.custom.promoMessage),
-  //   });
 };
