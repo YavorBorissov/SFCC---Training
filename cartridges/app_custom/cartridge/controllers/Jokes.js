3b935052a0496432ed19efe6996de44df45c9a6c
YavorBorissov <yavor.borissov@gmail.com>
2022-01-24 17:32:25 +0200

    added joke service and export job

--- cartridges/app_custom/cartridge/controllers/Jokes.js
+++ cartridges/app_custom/cartridge/controllers/Jokes.js
@@ -14,4 +14,23 @@ server.get("Show", function (req, res, next) {
   next();
 });
 
+server.get("Search", function (req, res, next) {
+  var properties = {};
+  var template = "jokeSearch";
+  var searchTerm = req.querystring.term || "";
+
+  var url = service.dadJokeAPIService.getURL() + "search";
+  var svcResult = service.dadJokeAPIService
+    .setURL(url)
+    .addParam("term", searchTerm)
+    .call();
+  if (svcResult.status === "OK") {
+    properties.term = searchTerm;
+    properties.jokes = svcResult.object.results;
+  }
+
+  res.render(template, properties);
+  next();
+});
+
 module.exports = server.exports();
