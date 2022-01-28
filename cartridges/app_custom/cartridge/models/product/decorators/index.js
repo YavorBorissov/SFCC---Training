ee21349c916c0e3bc3315bdd99753e35ecefa559
YavorBorissov <yavor.borissov@gmail.com>
2022-01-28 10:38:48 +0200

    added tags

--- cartridges/app_custom/cartridge/models/product/decorators/index.js
+++ cartridges/app_custom/cartridge/models/product/decorators/index.js
@@ -4,5 +4,6 @@ var base = module.superModule;
 
 base.promo = require("*/cartridge/models/product/decorators/promo");
 base.jokeTerm = require("*/cartridge/models/product/decorators/jokeTerm");
+base.tags = require("*/cartridge/models/product/decorators/tags");
 
 module.exports = base;
