73bd093705b26f9307a9f120d8737db2f396111c
YavorBorissov <yavor.borissov@gmail.com>
2022-02-03 17:42:47 +0200

    added userDetails form

---
+++ cartridges/app_custom/cartridge/controllers/UserDetails.js
@@ -0,0 +1,52 @@
+"use strict";
+var server = require("server");
+var URLUtils = require("dw/web/URLUtils");
+var csrfProtection = require("*/cartridge/scripts/middleware/csrf");
+var userLoggedIn = require("*/cartridge/scripts/middleware/userLoggedIn");
+
+server.get(
+  "Add",
+  server.middleware.https,
+  csrfProtection.generateToken,
+  userLoggedIn.validateLoggedIn,
+  function (req, res, next) {
+    var accountHelpers = require("*/cartridge/scripts/account/accountHelpers");
+    var accountModel = accountHelpers.getAccountModel(req);
+
+    var actionUrl = URLUtils.url("UserDetails-SaveDetails");
+    var userDetailsForm = server.forms.getForm("userDetails");
+    userDetailsForm.clear();
+
+    userDetailsForm.firstname.value = accountModel.profile.firstName;
+    userDetailsForm.lastname.value = accountModel.profile.lastName;
+    userDetailsForm.phone.value = accountModel.profile.phone;
+    userDetailsForm.username.value = accountModel.profile.username;
+    userDetailsForm.status.value = accountModel.profile.status;
+
+    res.render("userDetailsTemplate", {
+      actionUrl: actionUrl,
+      userDetailsForm: userDetailsForm,
+    });
+    next();
+  }
+);
+
+server.post("SaveDetails", server.middleware.https, function (req, res, next) {
+  var userDetailsForm = server.forms.getForm("userDetails");
+  var Transaction = require("dw/system/Transaction");
+  var CustomerMgr = require("dw/customer/CustomerMgr");
+
+  var customer = CustomerMgr.getCustomerByCustomerNumber(
+    req.currentCustomer.profile.customerNo
+  );
+
+  Transaction.wrap(function () {
+    customer.profile.firstName = userDetailsForm.firstname.value;
+    customer.profile.lastName = userDetailsForm.lastname.value;
+    customer.profile.phoneHome = userDetailsForm.phone.value;
+    customer.profile.custom.username = userDetailsForm.username.value;
+    customer.profile.custom.status = userDetailsForm.status.value;
+  });
+});
+
+module.exports = server.exports();
